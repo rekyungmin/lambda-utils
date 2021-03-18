e2a12752cb69f2369f249f060782c6ea5d83d818
Kyungmin Lee <rekyungmin@gmail.com>
2021-03-18 11:49:37 +0900

    :sparkles: add schema
    외부 API와 통신할 때 camel case가 필요하기 때문에,
    snake case<->camel case로 상호 변환이 가능하도록 설정

---
+++ lambda_utils/schema.py
@@ -0,0 +1,29 @@
+__all__ = ("BaseSchema",)
+
+import pydantic
+
+
+def camelize(s: str) -> str:
+    """
+    :example:
+        >>> camelize("hello_world")
+        'helloWorld'
+        >>> camelize("e_tag")
+        'eTag'
+        >>> camelize("_tag")
+        'Tag'
+    """
+    words = s.split("_")
+    return words[0] + "".join(word.capitalize() for word in words[1:])
+
+
+class BaseSchema(pydantic.BaseModel):
+    class Config:
+        alias_generator = camelize
+        allow_population_by_field_name = True
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
