6f6684d3b3d4cccabc65c91b8b5bab81d0660703
Kyungmin Lee <rekyungmin@gmail.com>
2021-03-18 15:48:38 +0900

    :sparkles: add a lambda invocation util

--- lambda_utils/__init__.py
+++ lambda_utils/__init__.py
@@ -1,3 +1,4 @@
 from lambda_utils import path
 from lambda_utils import schema
 from lambda_utils import s3storage
+from lambda_utils import function
---
+++ lambda_utils/function.py
@@ -0,0 +1,34 @@
+from __future__ import annotations
+
+__all__ = (
+    "InvocationType",
+    "invoke",
+)
+
+import enum
+from typing import Any, Optional
+
+from lambda_utils import _session
+
+
+class InvocationType(enum.Enum):
+    EVENT = "Event"
+    REQUEST_RESPONSE = "RequestResponse"
+    DRY_RUN = "DryRun"
+
+
+def invoke(
+    function_name: str,
+    invocation_type: InvocationType,
+    payload_json: str = "",
+    *,
+    session_config: Optional[dict[str, Any]] = None,
+) -> dict[str, Any]:
+    if session_config is None:
+        session_config = {}
+    session = _session.get_client("lambda", **session_config)
+    return session.invoke(
+        FunctionName=function_name,
+        InvocationType=invocation_type.value,
+        Payload=payload_json,
+    )
