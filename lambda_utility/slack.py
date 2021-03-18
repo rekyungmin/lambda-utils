3ff50ed54c766395389a7225d15ec97a164b6e3d
Kyungmin Lee <rekyungmin@gmail.com>
2021-03-18 18:27:30 +0900

    :sparkles: support message blocks

--- lambda_utility/slack.py
+++ lambda_utility/slack.py
@@ -2,13 +2,21 @@ from __future__ import annotations
 
 __all__ = ("send_message",)
 
+from typing import Union, Any
+
 import slack_sdk
 
 
-def send_message(token: str, channel: str, message: str) -> slack_sdk.web.SlackResponse:
+def send_message(
+    token: str, channel: str, message: Union[str, list[dict[str, Any]]]
+) -> slack_sdk.web.SlackResponse:
     """send a slack message
+
     :exception: slack_sdk.errors.SlackApiError
     """
     client = slack_sdk.WebClient(token=token)
-    response = client.chat_postMessage(channel=channel, text=message)
+    if isinstance(message, list):
+        response = client.chat_postMessage(channel=channel, blocks=message)
+    else:
+        response = client.chat_postMessage(channel=channel, text=message)
     return response
