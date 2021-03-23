c6d711d88f45e1b14d91fb5e2222fc80b8561ce5
Kyungmin Lee <rekyungmin@gmail.com>
2021-03-23 14:17:22 +0900

    :label: add 'LambdaContext' Protocol

--- lambda_utility/typedefs.py
+++ lambda_utility/typedefs.py
@@ -1,7 +1,42 @@
-__all__ = ("PathLike",)
+__all__ = (
+    "PathLike",
+    "CognitoIdentity",
+    "ClientContext",
+    "LambdaContext",
+)
 
-from typing import Union
+from typing import Union, Protocol, Optional
 
 from lambda_utility.path import PathExt
 
 PathLike = Union[str, PathExt]
+
+
+class CognitoIdentity(Protocol):
+    cognito_identity_id: Optional[str]
+    cognito_identity_pool_id: Optional[str]
+
+
+class ClientContext(Protocol):
+    installation_id: str
+    app_title: str
+    app_version_name: str
+    app_version_code: str
+    app_package_name: str
+    custom: dict
+    env: dict
+
+
+class LambdaContext(Protocol):
+    function_name: str
+    function_version: str
+    invoked_function_arn: str
+    memory_limit_in_mb: str
+    aws_request_id: str
+    log_group_name: str
+    log_stream_name: str
+    identity: CognitoIdentity
+    client_context: Optional[ClientContext]
+
+    def get_remaining_time_in_millis(self) -> int:
+        ...
