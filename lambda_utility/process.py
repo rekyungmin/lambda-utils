44bb853d5c8d118e7547913cf1fd2eb210dde20f
Kyungmin Lee <rekyungmin@gmail.com>
2021-05-30 21:18:32 +0900

    :sparkles: add template-based subprocess function

--- lambda_utility/process.py
+++ lambda_utility/process.py
@@ -4,13 +4,15 @@ __all__ = (
     "ProcessError",
     "optionize",
     "run_command",
+    "run_template_command",
 )
 
 import asyncio
 import enum
 import logging
 import pathlib
-import sys
+import shlex
+import string
 from typing import Optional, Union
 
 logger = logging.getLogger(__file__)
@@ -59,22 +61,12 @@ def optionize(*params: Union[str, tuple[str, Union[str, float, bool]]]) -> list[
 
 async def run_command(command: str, *params: str) -> tuple[bytes, bytes]:
     logger.debug("[Subprocess] run: '%s'", " ".join([command, *params]))
-    if sys.version_info < (3, 10):
-        loop = asyncio.get_running_loop()
-        proc = await asyncio.create_subprocess_exec(
-            command,
-            *params,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            loop=loop,
-        )
-    else:
-        proc = await asyncio.create_subprocess_exec(
-            command,
-            *params,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
+    proc = await asyncio.create_subprocess_exec(
+        command,
+        *params,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
     stdout, stderr = await proc.communicate()
 
     if proc.returncode != 0:
@@ -89,4 +81,31 @@ async def run_command(command: str, *params: str) -> tuple[bytes, bytes]:
     logger.debug("[Subprocess] stdout %s", stdout)
     logger.debug("[Subprocess] stderr %s", stderr)
 
+    return stdout, stderr
+
+
+async def run_template_command(
+    template: string.Template, **params: str
+) -> tuple[bytes, bytes]:
+    safe_kwargs = {key: shlex.quote(str(value)) for key, value in params.items()}
+    command = template.substitute(safe_kwargs)
+    logger.debug(f"[Subprocess] run: {command!r}")
+    proc = await asyncio.create_subprocess_shell(
+        command,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await proc.communicate()
+
+    if proc.returncode != 0:
+        raise ProcessError(
+            "Encoding failed",
+            proc.returncode,
+            command,
+            stdout,
+            stderr,
+        )
+
+    logger.debug("[Subprocess] stdout %s", stdout)
+    logger.debug("[Subprocess] stderr %s", stderr)
     return stdout, stderr
