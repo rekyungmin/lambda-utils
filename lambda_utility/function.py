4b7ae493a00c1df40ffcd4b826fc2a0b5f5273c4
Kyungmin Lee <rekyungmin@gmail.com>
2021-07-06 18:38:07 +0900

    :recycle: function error 여부를 파악할 때 헤더를 보도록 변경

--- lambda_utility/function.py
+++ lambda_utility/function.py
@@ -2,40 +2,65 @@ from __future__ import annotations
 
 __all__ = ("invoke",)
 
-from typing import Optional, Literal, Union, BinaryIO, Any
+from typing import Optional, Literal, Union, BinaryIO, Dict
 
 import aiobotocore
 import botocore.client
 
-from lambda_utility.session import create_client
 from lambda_utility.schema import LambdaInvocationResponse
-
-InvocationType = Literal["Event", "RequestResponse", "DryRun"]
-LogType = Literal["None", "Tail"]
+from lambda_utility.session import create_client
 
 
 class LambdaInvokeError(Exception):
     pass
 
 
-def is_success_response(payload: Any) -> bool:
-    if not isinstance(payload, dict):
-        return True
-
-    # TODO: 헤더 또는 다른 검증된 값을 이용하도록 변경 필요
-    return "errorMessage" not in payload
+def _is_success_response(header: Dict) -> bool:
+    return "x-amz-function-error" not in header
 
 
 async def invoke(
     function_name: str,
-    invocation_type: InvocationType,
+    invocation_type: Literal["Event", "RequestResponse", "DryRun"],
     payload: Union[bytes, BinaryIO],
-    log_type: LogType = "None",
+    log_type: Literal["None", "Tail"] = "None",
     *,
     client: Optional[aiobotocore.session.ClientCreatorContext] = None,
     config: Optional[botocore.client.Config] = None,
     raise_exception: bool = True,
 ) -> LambdaInvocationResponse:
+    """Invokes a Lambda function.
+    You can invoke a function synchronously (and wait for the response),
+    or asynchronously. To invoke a function asynchronously, set InvocationType to Event.
+
+    :ref: https://botocore.amazonaws.com/v1/documentation/api/latest/reference/services/lambda.html?highlight=invoke#Lambda.Client.invoke
+    :exception: Lambda.Client.exceptions.ServiceException
+    :exception: Lambda.Client.exceptions.ResourceNotFoundException
+    :exception: Lambda.Client.exceptions.InvalidRequestContentException
+    :exception: Lambda.Client.exceptions.RequestTooLargeException
+    :exception: Lambda.Client.exceptions.UnsupportedMediaTypeException
+    :exception: Lambda.Client.exceptions.TooManyRequestsException
+    :exception: Lambda.Client.exceptions.InvalidParameterValueException
+    :exception: Lambda.Client.exceptions.EC2UnexpectedException
+    :exception: Lambda.Client.exceptions.SubnetIPAddressLimitReachedException
+    :exception: Lambda.Client.exceptions.ENILimitReachedException
+    :exception: Lambda.Client.exceptions.EFSMountConnectivityException
+    :exception: Lambda.Client.exceptions.EFSMountFailureException
+    :exception: Lambda.Client.exceptions.EFSMountTimeoutException
+    :exception: Lambda.Client.exceptions.EFSIOException
+    :exception: Lambda.Client.exceptions.EC2ThrottledException
+    :exception: Lambda.Client.exceptions.EC2AccessDeniedException
+    :exception: Lambda.Client.exceptions.InvalidSubnetIDException
+    :exception: Lambda.Client.exceptions.InvalidSecurityGroupIDException
+    :exception: Lambda.Client.exceptions.InvalidZipFileException
+    :exception: Lambda.Client.exceptions.KMSDisabledException
+    :exception: Lambda.Client.exceptions.KMSInvalidStateException
+    :exception: Lambda.Client.exceptions.KMSAccessDeniedException
+    :exception: Lambda.Client.exceptions.KMSNotFoundException
+    :exception: Lambda.Client.exceptions.InvalidRuntimeException
+    :exception: Lambda.Client.exceptions.ResourceConflictException
+    :exception: Lambda.Client.exceptions.ResourceNotReadyException
+    """
     if client is None:
         client = create_client("lambda", config=config)
 
@@ -53,7 +78,9 @@ async def invoke(
             received_payload = None
 
         result = LambdaInvocationResponse(**resp, payload=received_payload)
-        if raise_exception and not is_success_response(result.payload):
-            raise LambdaInvokeError(str(result.payload))  # TODO: 메시지는 변경될 수 있음
+        if raise_exception and not _is_success_response(
+            resp["ResponseMetadata"]["HTTPHeaders"]
+        ):
+            raise LambdaInvokeError(str(result.payload))
 
         return result
