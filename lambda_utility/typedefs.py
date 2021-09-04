709e72d65514ea81ce14ef658f265b61d2ac9a6a
Kyungmin Lee <rekyungmin@gmail.com>
2021-09-04 23:51:27 +0900

    :label: botocore config 타입 추가

--- lambda_utility/typedefs.py
+++ lambda_utility/typedefs.py
@@ -3,14 +3,20 @@ __all__ = (
     "CognitoIdentity",
     "ClientContext",
     "LambdaContext",
+    "S3Config",
+    "RetriesConfig",
+    "ProxiesConfig",
+    "Config",
 )
 
 import os
-from typing import Union, Protocol, Optional
+from typing import Union, Protocol, Optional, TypedDict, Literal
 
 StrPath = Union[str, os.PathLike[str]]
 
 
+# AWS Lambda Context 정의 시작
+# https://docs.aws.amazon.com/lambda/latest/dg/python-context.html
 class CognitoIdentity(Protocol):
     cognito_identity_id: Optional[str]
     cognito_identity_pool_id: Optional[str]
@@ -39,3 +45,47 @@ class LambdaContext(Protocol):
 
     def get_remaining_time_in_millis(self) -> int:
         ...
+
+
+# AWS Lambda Context 정의 종료
+
+
+# botocore.config 정의 시작
+# https://botocore.amazonaws.com/v1/documentation/api/latest/reference/config.html
+class S3Config(TypedDict, total=False):
+    use_accelerate_endpoint: bool
+    payload_signing_enabled: bool
+    addressing_style: Literal["auto", "virtual", "path"]
+    us_east_1_regional_endpoint: Literal["regional", "legacy"]
+
+
+class RetriesConfig(TypedDict, total=False):
+    total_max_attempts: int
+    max_attempts: int
+    mode: Literal["legacy", "standard", "adaptive"]
+
+
+class ProxiesConfig(TypedDict, total=False):
+    proxy_ca_bundle: str
+    proxy_client_cert: Union[str, tuple[str, str]]
+    proxy_use_forwarding_for_https: bool
+
+
+class Config(TypedDict, total=False):
+    region_name: str
+    signature_version: str
+    user_agent: str
+    user_agent_extra: str
+    connect_timeout: float
+    read_timeout: float
+    parameter_validation: bool
+    max_pool_connections: int
+    proxies: dict[str, str]
+    proxies_config: ProxiesConfig
+    s3: S3Config
+    retries: RetriesConfig
+    client_cert: Union[str, tuple[str, str]]
+    inject_host_prefix: bool
+
+
+# botocore.config 정의 종료
