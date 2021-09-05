93081ea5d85303db246268fd87dd19a523e19ae8
Kyungmin Lee <rekyungmin@gmail.com>
2021-09-05 09:30:37 +0900

    :bookmark: 2.0.0-alpha

--- lambda_utility/__init__.py
+++ lambda_utility/__init__.py
@@ -1,14 +1,1 @@
-from lambda_utility import function
-from lambda_utility import image
-from lambda_utility import mp
-from lambda_utility import path
-from lambda_utility import process
-from lambda_utility import s3storage
-from lambda_utility import schema
-from lambda_utility import session
-from lambda_utility import sqs
-from lambda_utility import typedefs
-from lambda_utility import utils
-from lambda_utility import zipper
-
-__version__ = "1.14.0"
+__version__ = "2.0.0-alpha"
